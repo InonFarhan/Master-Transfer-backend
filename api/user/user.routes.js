7f6fbd55923173da0f077cce7d5c5db32fe88367
inon farhan <inon.f4@gmail.com>
2023-05-31 19:37:02 +0300

    update

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth.middleware')
+const { requireAuth } = require('../../middlewares/requireAuth.middleware')
 const { getUser, getUsers, deleteUser, updateUser } = require('./user.controller')
 const router = express.Router()
 
@@ -9,8 +9,6 @@ router.use(requireAuth)
 router.get('/', getUsers)
 router.get('/:id', getUser)
 router.put('/:id', requireAuth, updateUser)
-
-// router.put('/:id',  requireAuth, updateUser)
-router.delete('/:id', requireAuth, requireAdmin, deleteUser)
+router.delete('/:id', requireAuth, deleteUser)
 
 module.exports = router
--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -3,8 +3,7 @@ const config = require('../config')
 const asyncLocalStorage = require('../services/als.service')
 
 module.exports = {
-  requireAuth,
-  requireAdmin
+  requireAuth
 }
 
 function requireAuth(req, res, next) {
@@ -17,16 +16,5 @@ function requireAuth(req, res, next) {
   }
   if (!loggedinUser) return res.status(401).send('Not Authenticated')
   req.loggedinUser = loggedinUser
-  next()
-}
-
-function requireAdmin(req, res, next) {
-  const { loggedinUser } = asyncLocalStorage.getStore()
-  if (!loggedinUser) return res.status(401).send('Not Authenticated')
-  if (!loggedinUser.isAdmin) {
-    logger.warn(loggedinUser.fullname + 'attempted to perform admin action')
-    res.status(403).end('Not Authorized')
-    return
-  }
   next()
 }
