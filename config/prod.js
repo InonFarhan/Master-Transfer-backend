648acd9b6399afeeb6246838c6301332c0d5d9b9
inon farhan <inon.f4@gmail.com>
2023-06-01 17:17:39 +0300

    test

--- config/prod.js
+++ config/prod.js
@@ -3,3 +3,5 @@ module.exports = {
   dbURL: process.env.mongoURL,
   dbName: process.env.dbName
 }
+console.log('process.env.mongoURL', process.env.mongoURL)
+console.log('process.env.dbName', process.env.dbName)
