f7ac1a0f9f2a97c935f8e293400e28b0e333ca3f
inon farhan <inon.f4@gmail.com>
2023-06-01 17:34:32 +0300

    prod links

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
+// require('dotenv').config()
 // module.exports = {
 //   dbURL: process.env.mongoURL,
 //   dbName: process.env.dbName
 // }
 
 module.exports = {
-  dbURL: 'mongodb://127.0.0.1:27017/bitcoin_db',
+  // dbURL: 'mongodb://127.0.0.1:27017/bitcoin_db',
+  dbURL: process.env.MONGO_URL || 'mongodb://localhost:27017/bitcoin_db',
 }
