e8493395e75f093f659965e9633de82330f88c35
inon farhan <inon.f4@gmail.com>
2023-06-01 16:45:14 +0300

    prod link

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  dbURL: process.env.mongoURL,
-  dbName: process.env.dbName
+  // dbURL: process.env.mongoURL,
+  // dbName: process.env.dbName
+  dbURL: 'mongodb://127.0.0.1:27017/bitcoin_db',
 }
