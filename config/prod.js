8e23fba6daa500ae643b338f817ab1b3521fa12e
inon farhan <inon.f4@gmail.com>
2023-06-02 10:54:43 +0300

    prod links

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   dbName: 'master-transfer',
-  dbURL: 'mongodb+srv://inonf4:1397123@cluster0.gyfpaos.mongodb.net/'
+  dbURL: 'mongodb+srv://inonf4:1397123@cluster0.p8itddz.mongodb.net/'
 }
